971bb049b08f371c9b8af5569c5bf61bb0b6d0fc
sebashtioon <sebastiansuciu607@gmail.com>
2026-05-27 15:23:40 +1000

    update wave_query.glsl to use rgba16f format and define MapScales buffer

--- res/shad/compute/wave_query.glsl
+++ res/shad/compute/wave_query.glsl
@@ -9,6 +9,8 @@
 
 layout (local_size_x = LOCAL_SIZE, local_size_y = 1, local_size_z = 1) in;
 
-layout(rgb16f, set = 0, binding = 0) uniform readonly image2DArray displacement_map;
+layout(rgba16f, set = 0, binding = 0) uniform readonly image2DArray displacement_map;
 
-layout(rgb16f, set = 0, binding = 0) uniform readonly image2DArray displacement_map;
+layout(std430, set = 0, binding = 1) restrict readonly buffer MapScales {
+    vec4 map_scales[MAX_CASCADES];
+}

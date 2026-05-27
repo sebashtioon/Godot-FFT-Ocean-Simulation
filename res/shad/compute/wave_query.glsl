31a01003b4ca9042a712d3b744e098b68758cf1e
sebashtioon <sebastiansuciu607@gmail.com>
2026-05-27 15:22:37 +1000

    add more layouts

--- res/shad/compute/wave_query.glsl
+++ res/shad/compute/wave_query.glsl
@@ -8,3 +8,7 @@
 
 
 layout (local_size_x = LOCAL_SIZE, local_size_y = 1, local_size_z = 1) in;
+
+layout(rgb16f, set = 0, binding = 0) uniform readonly image2DArray displacement_map;
+
+layout(rgb16f, set = 0, binding = 0) uniform readonly image2DArray displacement_map;
